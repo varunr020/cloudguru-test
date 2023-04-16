acf28272c7d0d00359c72f385b3c09b15278260c
varunr020 <105972093+varunr020@users.noreply.github.com>
2023-04-16 18:46:18 +0530

    Create addtwonum.sh
    added new file

---
+++ addtwonum.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "This is scripting Job is from GitHub"
+echo "This is not yet fully automatic"
+NUM=$(($NUM1 + $NUM2))
+echo "Additiom of $NUM1 and $NUM2 is $NUM"
+echo "Thank you ..."
