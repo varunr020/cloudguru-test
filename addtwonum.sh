6fa346bd33be0036fa7fb3ab818adb0a586a4fe1
varun <varunr020@gmail.com>
2023-04-19 13:40:07 +0530

    change from local system

--- addtwonum.sh
+++ addtwonum.sh
@@ -3,4 +3,4 @@ echo "This is scripting Job is from GitHub"
 echo "This is now fully automatic"
 NUM=$(($NUM1 + $NUM2))
 echo "Additiom of $NUM1 and $NUM2 is $NUM"
-echo "Thank you !!!"
+echo "Thank you ..!"
